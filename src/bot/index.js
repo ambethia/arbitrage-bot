f7af29addf62ab835b913c743f2fa93b162c7512
Jason L Perry <jasper@ambethia.com>
2017-06-15 23:47:11 -0400

    hmm

--- src/bot/index.js
+++ src/bot/index.js
@@ -10,9 +10,9 @@ class Bot {
 
   constructor () {
     this.engine.on('update', (product) => {
-      const { base, quote, fee, bid, ask } = product
-      this.updateGraph(base, quote, bid.price, fee)
-      this.updateGraph(quote, base, ask.price, fee)
+      const { base, quote, fee, ask } = product
+      this.updateGraph(base, quote, ask.price, fee)
+      this.updateGraph(quote, base, num(1).div(ask.price), fee)
       this.findArbitrage()
     })
   }
@@ -29,45 +29,62 @@ class Bot {
   findArbitrage () {
     const dist = {}
     const pred = {}
-    Object.keys(this.graph).forEach(v => { dist[v] = Number.MAX_VALUE })
-    dist.ROOT = 0
+    Object.keys(this.graph).forEach(v => { dist[v] = num(Number.MAX_VALUE) })
+    dist.ROOT = num(0)
 
     const n = Object.keys(this.graph).length
     // Relax every edge n - 1 times
     _.times(n - 1, () => {
-      _.each(this.graph, (v1, e) => {
-        _.each(e, (v2, w) => {
-          if (dist[v2] > dist[v1] + w) {
-            dist[v2] = dist[v1] + w
+      _.each(this.graph, (e, v1) => {
+        _.each(e, (w, v2) => {
+          if (dist[v2].gt(dist[v1].add(w))) {
+            dist[v2] = dist[v1].add(w)
             pred[v2] = v1
           }
         })
       })
     })
 
-    console.log(this.graph)
-
     // Relax every edge again to find negative-weight cycles
-    // let arbitrage = false
-    // const cyclic = {}
-    // arbitrage = false
-    // cyclic = {}
-    // graph.each do |v_1, e|
-    //   e.each do |v_2, w|
-    //     if dist[v_2] > dist[v_1] + w
-    //       arbitrage = true
-    //       dist[v_2] = dist[v_1] + w
-    //
-    //       # Keep track of vertices in negative-weight cycles
-    //       cyclic[v_2] = true
-    //     end
-    //   end
-    // end
-    //
-    // if !arbitrage
-    //   puts "No arbitrage found."
-    //   exit
-    // end
+    let arbitrage = false
+    const cyclic = {}
+    _.each(this.graph, (e, v1) => {
+      _.each(e, (w, v2) => {
+        if (dist[v2].gt(dist[v1].add(w))) {
+          arbitrage = true
+          dist[v2] = dist[v1].add(w)
+          cyclic[v2] = true
+        }
+      })
+    })
+
+    if (!arbitrage) {
+      console.log('No arbitrage found')
+      return
+    }
+
+    // Calculate the arbitrage sequences
+    const sequences = []
+    Object.keys(cyclic).forEach((v) => {
+      // Recursively visit predecessors to trace vertices in cycle
+      const visited = { [v]: true }
+      const seq = []
+      let p = v
+      do {
+        seq.push(p)
+        visited[p] = true
+        p = pred[p]
+      } while (p && !visited[p])
+      seq.reverse().push(seq[0])
+      // Calculate the arbitrage amount
+      const val = _.range(0, seq.length - 2).reduce((v, i) => {
+        console.log(this.rates[seq[i]][seq[i + 1]].toString())
+        return v.mul(this.rates[seq[i]][seq[i + 1]])
+      }, num(1.0))
+      sequences.push({ currencies: seq, value: val.toString() })
+
+      console.log(sequences)
+    })
   }
 
   run () {
